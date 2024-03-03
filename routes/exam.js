e0b656d9cac5c2e881b2d528c5ed4b82ad7fa11b
Nursultan <nurrs.serkul@gmail.com>
2024-03-03 16:45:27 +0500

    swagger was added

--- routes/exam.js
+++ routes/exam.js
@@ -1,3 +1,10 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Exams
+ *   description: API endpoints for managing exams
+ */
+
 const router = require('express').Router();
 const _ = require('lodash');
 const Exams = require('../models/Exams');
@@ -8,6 +15,24 @@ const errorHandler = require('../middleware/errorHandler');
 const exams = new ApiOptimizer(Exams);
 const modelName = 'Exams';
 
+/**
+ * @swagger
+ * /exams:
+ *   get:
+ *     summary: Get all exams
+ *     tags: [Exams]
+ *     description: Retrieve a list of all exams.
+ *     responses:
+ *       200:
+ *         description: A list of exams
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Exam'
+ */
+
 router.route('/').get(async (req, res) => {
   try {
     await exams.getAll(req, res);
@@ -16,6 +41,31 @@ router.route('/').get(async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /exams/{id}:
+ *   get:
+ *     summary: Get an exam by ID
+ *     tags: [Exams]
+ *     description: Retrieve an exam by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The exam object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Exam'
+ *       404:
+ *         description: Exam not found
+ */
+
 router.route('/:id').get(async (req, res) => {
   try {
     await exams.getById(req, res, modelName);
@@ -23,6 +73,33 @@ router.route('/:id').get(async (req, res) => {
     errorHandler(err, req, res);
   }
 });
+
+/**
+ * @swagger
+ * /exams/add:
+ *   post:
+ *     summary: Add a new exam
+ *     tags: [Exams]
+ *     description: Add a new exam with the specified title.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *             example:
+ *               title: New Exam
+ *     responses:
+ *       200:
+ *         description: The newly created exam
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Exam'
+ */
 
 router.post('/add', async (req, res) => {
   try {
@@ -33,6 +110,42 @@ router.post('/add', async (req, res) => {
     errorHandler(err, req, res);
   }
 });
+
+/**
+ * @swagger
+ * /exams/{id}:
+ *   put:
+ *     summary: Update an exam by ID
+ *     tags: [Exams]
+ *     description: Update an exam by its ID with a new word.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               word:
+ *                 type: string
+ *             example:
+ *               word: NewWord
+ *     responses:
+ *       200:
+ *         description: The updated exam object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Exam'
+ *       404:
+ *         description: Exam not found
+ */
 
 router.route('/:id').put(async (req, res) => {
   try {
@@ -45,6 +158,31 @@ router.route('/:id').put(async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /exams/{id}:
+ *   delete:
+ *     summary: Delete an exam by ID
+ *     tags: [Exams]
+ *     description: Delete an exam by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: The deleted exam object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Exam'
+ *       404:
+ *         description: Exam not found
+ */
+
 router.route('/:id').delete(async (req, res) => {
   try {
     await exams.deleteById(req, res, modelName);
@@ -52,6 +190,42 @@ router.route('/:id').delete(async (req, res) => {
     errorHandler(err, req, res);
   }
 });
+
+/**
+ * @swagger
+ * /exams/{id}/words:
+ *   post:
+ *     summary: Add a word to an exam
+ *     tags: [Exams]
+ *     description: Add a word to an exam identified by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               word:
+ *                 type: string
+ *             example:
+ *               word: NewWord
+ *     responses:
+ *       200:
+ *         description: The updated exam object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Exam'
+ *       404:
+ *         description: Exam not found
+ */
 
 router.post('/:id/words', examController.addWordToExam);
 
--- routes/result.js
+++ routes/result.js
@@ -2,7 +2,100 @@ const router = require('express').Router();
 const _ = require('lodash');
 const examController = require('../controllers/examController');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Result
+ *   description: API endpoints for exam results
+ */
+
+/**
+ * @swagger
+ * /exams/{examId}:
+ *   post:
+ *     summary: Submit a word for an exam
+ *     description: Submit a word for the specified exam.
+ *     tags: [Result]
+ *     parameters:
+ *       - in: path
+ *         name: examId
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *               spokenWord:
+ *                 type: string
+ *             example:
+ *               userId: 123456
+ *               spokenWord: Hello
+ *     responses:
+ *       200:
+ *         description: Successful submission
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 submissionId:
+ *                   type: string
+ *       400:
+ *         description: Bad request, missing user ID or spoken word
+ *       404:
+ *         description: Exam not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post('/:examId', examController.submitWord);
+
+/**
+ * @swagger
+ * /exams/{examId}/submission/{submissionId}:
+ *   post:
+ *     summary: Get results for a submission
+ *     description: Get results for the specified submission of an exam.
+ *     tags: [Result]
+ *     parameters:
+ *       - in: path
+ *         name: examId
+ *         required: true
+ *         description: ID of the exam
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: submissionId
+ *         required: true
+ *         description: ID of the submission
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Results of the submission
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 [char]:
+ *                   type: string
+ *                   description: Percentage of correct answers for each character
+ *       404:
+ *         description: Submission not found
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post('/:examId/submission/:submissionId', examController.getResults);
 
 module.exports = router;
